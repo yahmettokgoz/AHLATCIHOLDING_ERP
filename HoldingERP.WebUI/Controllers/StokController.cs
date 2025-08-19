20c0cd1ad173f8a3d5d54b2b59f871539b13eef9
Ahmet Tokgöz <ahmetyusuftokgoz@gmail.com>
2025-08-19 13:17:36 +0300

    Envanterden ürün tüketildiğinde stoktan düşen StokCikisViewModel yapısı eklendi

--- HoldingERP.WebUI/Controllers/StokController.cs
+++ HoldingERP.WebUI/Controllers/StokController.cs
@@ -1,5 +1,8 @@
 ﻿using HoldingERP.Business.Abstract;
+using HoldingERP.Entities.Concrete;
+using HoldingERP.WebUI.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -10,10 +13,16 @@ namespace HoldingERP.WebUI.Controllers
     public class StokController : Controller
     {
         private readonly IStokService _stokService;
+        private readonly IDepartmanService _departmanService; 
+        private readonly IStokHareketiService _stokHareketiService; 
+        private readonly UserManager<Kullanici> _userManager;
 
-        public StokController(IStokService stokService)
+        public StokController(IStokService stokService, IDepartmanService departmanService, IStokHareketiService stokHareketiService, UserManager<Kullanici> userManager) // Güncellendi
         {
             _stokService = stokService;
+            _departmanService = departmanService;
+            _stokHareketiService = stokHareketiService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -23,5 +32,61 @@ namespace HoldingERP.WebUI.Controllers
                                           .ToList();
             return View(stokListesi);
         }
+
+        public IActionResult StokCikis(int id)
+        {
+            var stok = _stokService.GetAll().Include(s => s.Urun).FirstOrDefault(s => s.Id == id);
+            if (stok == null) return NotFound();
+
+            var model = new StokCikisViewModel
+            {
+                StokId = stok.Id,
+                UrunAdi = stok.Urun.Ad,
+                MevcutMiktar = stok.Miktar,
+                Departmanlar = _departmanService.GetAll().ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> StokCikis(StokCikisViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var stok = _stokService.GetById(model.StokId);
+                if (stok == null) return NotFound();
+
+                if (model.CikisMiktari > stok.Miktar)
+                {
+                    ModelState.AddModelError("CikisMiktari", "Çıkış miktarı mevcut stoktan fazla olamaz.");
+                    model.Departmanlar = _departmanService.GetAll().ToList(); 
+                    return View(model);
+                }
+
+                var currentUser = await _userManager.GetUserAsync(User);
+
+                stok.Miktar -= model.CikisMiktari;
+                stok.GuncellemeTarihi = DateTime.Now;
+                _stokService.Update(stok);
+
+                var hareket = new StokHareketi
+                {
+                    UrunId = stok.UrunId,
+                    Miktar = -model.CikisMiktari, 
+                    IslemTuru = IslemTuru.Cikis,
+                    Tarih = DateTime.Now,
+                    IslemiYapanKullaniciId = currentUser.Id,
+                };
+                _stokHareketiService.Create(hareket);
+                _stokService.SaveChanges();
+
+                TempData["SuccessMessage"] = $"{model.CikisMiktari} adet {model.UrunAdi} stoktan başarıyla düşüldü.";
+                return RedirectToAction("Index");
+            }
+            model.Departmanlar = _departmanService.GetAll().ToList();
+            return View(model);
+        }
+
     }
 }
---
+++ HoldingERP.WebUI/Models/StokCikisViewModel.cs
@@ -0,0 +1,25 @@
+﻿using HoldingERP.Entities.Concrete;
+using System.ComponentModel.DataAnnotations;
+
+namespace HoldingERP.WebUI.Models
+{
+    public class StokCikisViewModel
+    {
+
+        public int StokId { get; set; }
+        public string UrunAdi { get; set; }
+        public decimal MevcutMiktar { get; set; }
+
+        [Required(ErrorMessage = "Miktar girilmelidir.")]
+        [Display(Name = "Çıkış Yapılacak Miktar")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Miktar 0'dan büyük olmalıdır.")]
+        public decimal CikisMiktari { get; set; }
+
+        [Required(ErrorMessage = "Departman seçilmelidir.")]
+        [Display(Name = "Hangi Departman İçin")]
+        public int DepartmanId { get; set; }
+
+        public IEnumerable<Departman>? Departmanlar { get; set; }
+
+    }
+}
