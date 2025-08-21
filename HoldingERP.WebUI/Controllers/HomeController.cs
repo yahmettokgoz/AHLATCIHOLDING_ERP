42ae2b59d52d2790ea6afcf473f7fc70e529c954
Ahmet Tokgöz <ahmetyusuftokgoz@gmail.com>
2025-08-21 10:20:36 +0300

    Bootstrap ERP template uygulanarak frontend modern hale getirildi

--- HoldingERP.WebUI/Controllers/HomeController.cs
+++ HoldingERP.WebUI/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
+using HoldingERP.Business.Abstract;
+using HoldingERP.Entities.Concrete;
 using HoldingERP.WebUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using Microsoft.AspNetCore.Authorization;
 
 namespace HoldingERP.WebUI.Controllers
 {
@@ -9,15 +12,55 @@ namespace HoldingERP.WebUI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<Kullanici> _userManager;
+        private readonly ITalepService _talepService;
+        private readonly IStokService _stokService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<Kullanici> userManager, ITalepService talepService, IStokService stokService)
         {
             _logger = logger;
+            _userManager = userManager;
+            _talepService = talepService;
+            _stokService = stokService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.GetUserAsync(User);
+            var model = new DashboardViewModel();
+
+            // Tüm roller için ortak veriler
+            model.ToplamKullaniciSayisi = _userManager.Users.Count();
+            model.ToplamUrunCesidi = _stokService.GetAll().Count(); // Veya UrunService'den
+
+            // --- Role Göre Veri Doldurma ---
+
+            if (User.IsInRole("Admin") || User.IsInRole("Onaycý"))
+            {
+                model.OnayBekleyenTalepSayisi = _talepService.Find(t =>
+                    t.Durum == TalepDurumu.AmirOnayiBekliyor || t.Durum == TalepDurumu.YoneticiOnayiBekliyor).Count();
+            }
+
+            if (User.IsInRole("Satýn Almacý"))
+            {
+                model.IslemBekleyenSatinAlmaTalepSayisi = _talepService.Find(t => t.Durum == TalepDurumu.SatýnAlmada).Count();
+                model.TeklifBekleyenTalepSayisi = _talepService.Find(t => t.Durum == TalepDurumu.TeklifBekleniyor).Count();
+            }
+
+            if (User.IsInRole("Muhasebe"))
+            {
+                model.FaturaBekleyenTalepSayisi = _talepService.Find(t => t.Durum == TalepDurumu.Onaylandi).Count();
+            }
+
+            if (User.IsInRole("Talep Eden"))
+            {
+                model.KullanicininAktifTalepSayisi = _talepService.Find(t => t.TalepEdenKullaniciId == currentUser.Id && t.Durum != TalepDurumu.Tamamlandi).Count();
+            }
+
+            // Son 5 talebi de ekleyelim
+            model.SonTalepler = _talepService.GetAll().OrderByDescending(t => t.TalepTarihi).Take(5).ToList();
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ HoldingERP.WebUI/Models/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HoldingERP.Entities.Concrete;
+
+namespace HoldingERP.WebUI.Models
+{
+    public class DashboardViewModel
+    {
+        
+        public int ToplamKullaniciSayisi { get; set; }
+        public int OnayBekleyenTalepSayisi { get; set; }
+        public int KullanicininAktifTalepSayisi { get; set; }
+        public int IslemBekleyenSatinAlmaTalepSayisi { get; set; }
+        public int TeklifBekleyenTalepSayisi { get; set; }
+        public int FaturaBekleyenTalepSayisi { get; set; }
+        public int ToplamUrunCesidi { get; set; }
+        public IEnumerable<SatinAlmaTalebi>? SonTalepler { get; set; }
+    }
+}
